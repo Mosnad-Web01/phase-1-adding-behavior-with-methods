7e5e480ccf7ee4cc84fd20962f3993a2ee0cd0cc
Ahmed Nabil <bplahmed28d@gmail.com>
2024-08-26 14:36:14 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+class Cat {
+  constructor(name) {
+    this.speak = () => `${name} says meow!`;
+  }
+}
+class Dog {
+  constructor(name) {
+    this.speak = () => `${name} says woof!`;
+  }
+}
+class Bird {
+  constructor(name, sex) {
+    this.speak = () =>
+      sex === "male" ? `It's me! ${name}, the parrot!` : `${name} says squawk!`;
+  }
+}
